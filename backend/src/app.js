28536823051fb1ed6604562a8f0dcacf07f7f71e
unknown <solah0980@gmail.com>
2020-03-27 20:22:00 +0700

    'newupdate'

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const con = require('./config/config')
 const cors = require('cors')
+const port = process.env.POTT || 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(cors())
@@ -13,6 +14,6 @@ con.connect((err)=>{
 })
 
 require('./route')(app)
-app.listen(8081,(req,res)=>{
+app.listen(port,(req,res)=>{
     console.log('server open success')
 })
